6ca4eadeebd60b0a9e34042d980d0009a4cdaac8
Lionel Acosta <lionelx87@gmail.com>
2023-05-01 15:49:04 -0300

    feat(firebase): user login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { UserLogin } from "src/app/models/user.model";
+import { AuthService } from "src/app/services/auth.service";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styles: []
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styles: [],
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
-  ngOnInit() {
+  get validEmail() {
+    return this.loginForm.get("email").valid;
   }
 
+  get validPassword() {
+    return this.loginForm.get("password").valid;
+  }
+
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required]],
+    });
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      const { email, password }: UserLogin = this.loginForm.value;
+      this.authService.login({ email, password }).then((userCredential) => {
+        console.log(userCredential);
+        this.router.navigate(["/"]);
+      });
+    }
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from "@angular/core";
-import { UserRegister } from "../models/user.model";
+import { UserLogin, UserRegister } from "../models/user.model";
 import {
   Auth,
   getAuth,
   createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
 } from "@angular/fire/auth";
 
 @Injectable({
@@ -17,10 +18,10 @@ export class AuthService {
     const auth = getAuth();
     return createUserWithEmailAndPassword(auth, user.email, user.password);
   }
-}
 
-// createUser(user: UserRegister) {
-//   console.log(user);
-//   const auth = getAuth();
-//   return signInWithEmailAndPassword(auth, user.email, user.password);
-// }
+  login(user: UserLogin) {
+    console.log(user);
+    const auth = getAuth();
+    return signInWithEmailAndPassword(auth, user.email, user.password);
+  }
+}
