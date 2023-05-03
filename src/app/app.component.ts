24851c659babf1d102396993e00360fe87fd9821
Lionel Acosta <lionelx87@gmail.com>
2023-05-02 22:07:37 -0300

    feat(loading): global loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { AuthService } from "./services/auth.service";
+import { AppState } from "./app.reducer";
+import { Store } from "@ngrx/store";
+import { Observable, map } from "rxjs";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  constructor(private authService: AuthService) {
+export class AppComponent implements OnInit {
+  isLoading$: Observable<boolean>;
+
+  constructor(
+    private authService: AuthService,
+    private store: Store<AppState>
+  ) {
     this.authService.initAuthListener();
+  }
+
+  ngOnInit(): void {
+    this.isLoading$ = this.store.select("ui").pipe(map((ui) => ui.isLoading));
   }
 }
