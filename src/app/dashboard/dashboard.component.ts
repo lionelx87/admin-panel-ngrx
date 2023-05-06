8877e7d472a7dc6239d14f618390d237fe889478
Lionel Acosta <lionelx87@gmail.com>
2023-05-06 16:02:01 -0300

    feat(firestore): get items for collection

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '../app.reducer';
+import { Subscription, filter, pipe } from 'rxjs';
+import { EntryExitService } from '../services/entry-exit.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styles: []
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
+  authSub: Subscription;
 
-  constructor() { }
+  constructor(private store: Store<AppState>, private entryExitService: EntryExitService) { }
 
   ngOnInit() {
+    this.authSub = this.store.select('auth').pipe(
+      filter(auth => auth.user !== null)
+    ).subscribe( ({user}) => {
+      this.entryExitService.initEntryExitListener( user.uid );
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.authSub.unsubscribe();
   }
 
 }
--- src/app/services/entry-exit.service.ts
+++ src/app/services/entry-exit.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from "@angular/core";
-import { Firestore, addDoc, collection } from "@angular/fire/firestore";
+import { Firestore, addDoc, collection, doc, getDoc, onSnapshot } from "@angular/fire/firestore";
 import { EntryExit } from "../models/entry-exit.model";
 import { AuthService } from "./auth.service";
+import { getAuth } from "@angular/fire/auth";
 
 @Injectable({
   providedIn: "root",
@@ -13,5 +14,14 @@ export class EntryExitService {
     const uid = this.authService.user.uid;
     const prueba = collection(this.db, uid);
     return addDoc(collection(prueba, "entry-exit", "items"), entryExit);
+  }
+
+  async initEntryExitListener(uid: string) {
+    onSnapshot(collection(this.db, uid, "entry-exit", 'items'), (docs) => {
+      docs.forEach( (doc) => {
+        console.log('id: ', doc.id);
+        console.log('data: ', doc.data());
+      });
+    })
   }
 }
