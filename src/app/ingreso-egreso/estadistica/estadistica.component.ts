75ef80be1808dd31fba999f03d0f25b412e10bad
Lionel Acosta <lionelx87@gmail.com>
2023-05-06 23:18:33 -0300

    feat(dashboard): generate statistics

--- src/app/ingreso-egreso/estadistica/estadistica.component.ts
+++ src/app/ingreso-egreso/estadistica/estadistica.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import { EntryExit } from 'src/app/models/entry-exit.model';
 
 @Component({
   selector: 'app-estadistica',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EstadisticaComponent implements OnInit {
 
-  constructor() { }
+  entry: number = 0;
+  exit: number = 0;
+  totalEntry: number = 0;
+  totalExit: number = 0;
+
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
+    this.store.select('entryExit').subscribe( ({ items }) => this.generateStatistics(items))
+  }
+
+  generateStatistics(items: EntryExit[]) {
+    for(const item of items) {
+      if(item.type === 'entry') {
+        this.totalEntry += item.amount;
+        this.entry ++;
+      }else {
+        this.totalExit += item.amount;
+        this.exit ++;
+      }
+    }
+
   }
 
 }
