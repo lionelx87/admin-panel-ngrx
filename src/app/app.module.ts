745bd34f533bb1921e519e13943a97091bb539f1
Lionel Acosta <lionelx87@gmail.com>
2023-05-10 00:01:38 -0300

    fix: deploy in firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from "@angular/core";
+import { NgModule, isDevMode } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 // Modulos
@@ -8,11 +8,11 @@ import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
 import { getAuth, provideAuth } from "@angular/fire/auth";
 import { getFirestore, provideFirestore } from "@angular/fire/firestore";
 import { StoreModule } from "@ngrx/store";
-import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 import { environment } from "../environments/environment";
 import { AppComponent } from "./app.component";
 import { appReducers } from "./app.reducer";
 import { AuthModule } from "./auth/auth.module";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 
 @NgModule({
   declarations: [AppComponent],
@@ -24,11 +24,13 @@ import { AuthModule } from "./auth/auth.module";
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
     StoreModule.forRoot(appReducers),
-    StoreDevtoolsModule.instrument({
-      maxAge: 25, // Retains last 25 states
-      logOnly: environment.production, // Restrict extension to log-only mode
-      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
-    }),
+    !environment.production
+      ? StoreDevtoolsModule.instrument({
+          maxAge: 25, // Retains last 25 states
+          logOnly: !isDevMode(), // Restrict extension to log-only mode
+          autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+        })
+      : [],
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,14 @@
 
 export const environment = {
   firebase: {
-    projectId: 'admin-ngrx',
-    appId: '1:73222602327:web:05ad4205558eff7090ad12',
-    storageBucket: 'admin-ngrx.appspot.com',
-    apiKey: 'AIzaSyCtI8WYPlVHE8WXE_qtfWKTken-n70UYW8',
-    authDomain: 'admin-ngrx.firebaseapp.com',
-    messagingSenderId: '73222602327',
+    projectId: "admin-ngrx",
+    appId: "1:73222602327:web:05ad4205558eff7090ad12",
+    storageBucket: "admin-ngrx.appspot.com",
+    apiKey: "AIzaSyCtI8WYPlVHE8WXE_qtfWKTken-n70UYW8",
+    authDomain: "admin-ngrx.firebaseapp.com",
+    messagingSenderId: "73222602327",
   },
-  production: false
+  production: true,
 };
 
 /*
