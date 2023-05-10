bcb4b21eb2b2dec77d2e46482213796993f5c6b9
Lionel Acosta <lionelx87@gmail.com>
2023-05-09 22:35:25 -0300

    feat(lazy-load): canload implement

--- src/app/dashboard/dashboard-routes.module.ts
+++ src/app/dashboard/dashboard-routes.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { DashboardComponent } from "./dashboard.component";
 import { dashboardRoutes } from "./dashboard.routes";
+import { AuthGuard } from "../services/auth.guard";
 
 const routesChild: Routes = [
   {
     path: "",
     component: DashboardComponent,
     children: dashboardRoutes,
-    // canActivate: [AuthGuard],
+    // canLoad: [AuthGuard],
   },
 ];
 
--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  CanLoad,
   Router,
   RouterStateSnapshot,
   UrlTree,
@@ -12,8 +13,18 @@ import { AuthService } from "./auth.service";
 @Injectable({
   providedIn: "root",
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
   constructor(private authService: AuthService, private router: Router) {}
+
+  canLoad(): Promise<boolean> | boolean {
+    return this.authService.isAuth().then((isLogged) => {
+      if (!isLogged) {
+        this.router.navigate(["/login"]);
+        return;
+      }
+      return isLogged;
+    });
+  }
 
   canActivate(): Promise<boolean> {
     return this.authService.isAuth().then((isLogged) => {
